e4d988e6f1eef7f2f124b1bc1bd39e39834d8ffb
Eddie Chan <eddie.chan@connect.ust.hk>
2026-05-12 01:33:37 +0800

    progress: 05-11

--- exercises/05_ticket_v2/01_enum/src/lib.rs
+++ exercises/05_ticket_v2/01_enum/src/lib.rs
@@ -7,15 +7,31 @@
 struct Ticket {
     title: String,
     description: String,
-    status: String,
+    status: Status,
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Status {
-    // TODO: add the missing variants
+    ToDo,
+    InProgress,
+    Done,
+}
+
+impl Status{
+    fn to_string(&self) -> &str {
+        match self {
+            Status::ToDo => "ToDo",
+            Status::InProgress => "InProgress",
+            Status::Done => "Done",
+        }
+    }
+    fn clone (status: &Status) -> Status {
+        status.clone()
+    }
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
+    pub fn new(title: String, description: String, status: Status) -> Ticket {
         if title.is_empty() {
             panic!("Title cannot be empty");
         }
@@ -27,9 +43,6 @@ impl Ticket {
         }
         if description.len() > 500 {
             panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
 
         Ticket {
@@ -47,7 +60,7 @@ impl Ticket {
         &self.description
     }
 
-    pub fn status(&self) -> &String {
+    pub fn status(&self) -> &Status {
         &self.status
     }
 }
--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -1,8 +1,15 @@
 // TODO: `easy_ticket` should panic when the title is invalid.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
+
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    let description = if description.is_empty() || description.len() > 500 {
+        "Description not provided".to_string()
+    } else {
+        description
+    };
+
+    Ticket::new(title, description, status).unwrap()
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -26,12 +33,6 @@ impl Ticket {
         }
         if title.len() > 50 {
             return Err("Title cannot be longer than 50 bytes".to_string());
-        }
-        if description.is_empty() {
-            return Err("Description cannot be empty".to_string());
-        }
-        if description.len() > 500 {
-            return Err("Description cannot be longer than 500 bytes".to_string());
         }
 
         Ok(Ticket {
