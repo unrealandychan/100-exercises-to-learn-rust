298c0cbe0d430702db110a7ab54bc128792360a1
Eddie Chan <eddie.chan@connect.ust.hk>
2026-05-13 23:49:30 +0800

    progress: chapter 6-7, so difficult

--- exercises/06_ticket_management/00_intro/src/lib.rs
+++ exercises/06_ticket_management/00_intro/src/lib.rs
@@ -1,6 +1,6 @@
 fn intro() -> &'static str {
-    // TODO: fix me 👇
-    "I'm ready to __!"
+
+    "I'm ready to build a ticket management system!"
 }
 
 #[cfg(test)]
--- exercises/06_ticket_management/06_lifetimes/src/lib.rs
+++ exercises/06_ticket_management/06_lifetimes/src/lib.rs
@@ -31,7 +31,25 @@ impl TicketStore {
         self.tickets.push(ticket);
     }
 
-    pub fn iter(&self) -> std::slice::Iter<Ticket> {
+    pub fn iter(&self) -> std::slice::Iter<'_, Ticket> {
+        self.tickets.iter()
+    }
+}
+
+impl IntoIterator for TicketStore {
+    type Item = Ticket;
+    type IntoIter = std::vec::IntoIter<Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.tickets.into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = std::slice::Iter<'a, Ticket>;
+
+    fn into_iter(self) -> Self::IntoIter {
         self.tickets.iter()
     }
 }
